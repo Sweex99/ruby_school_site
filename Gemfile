5b839a13c9dc1a4e99c5993b7d659b66780fcd06
Taras <swex99@gmail.com>
2018-11-28 21:45:37 +0200

    new commit

--- Gemfile
+++ Gemfile
@@ -5,14 +5,14 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'nokogiri'
-
 gem 'will_paginate', '~> 3.1', '>= 3.1.6'
 gem 'will_paginate-bootstrap'
 
 gem 'rolify'
 gem 'devise'
 gem 'cancan'
+
+gem 'pg'
 
 gem 'letter_opener', :group => :development
 
@@ -21,7 +21,7 @@ gem 'bootstrap', '~> 4.1.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
 # Use Postgres as the database for Active Record
-gem 'sqlite3'
+#gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181125135342) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "home_tasks", force: :cascade do |t|
     t.string "description"
     t.string "subject"
@@ -22,12 +25,12 @@ ActiveRecord::Schema.define(version: 20181125135342) do
   end
 
   create_table "home_works", force: :cascade do |t|
-    t.string "monday"
-    t.string "tuesday"
-    t.string "wednesday"
-    t.string "thursday"
-    t.string "friday"
-    t.string "saturday"
+    t.string "monday", null: false
+    t.string "tuesday", null: false
+    t.string "wednesday", null: false
+    t.string "thursday", null: false
+    t.string "friday", null: false
+    t.string "saturday", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -52,6 +55,7 @@ ActiveRecord::Schema.define(version: 20181125135342) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -63,7 +67,6 @@ ActiveRecord::Schema.define(version: 20181125135342) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
