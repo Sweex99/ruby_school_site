4043b84aa68cf44846367d90ddc876cd601b0391
Taras <swex99@gmail.com>
2019-03-05 12:49:59 +0200

    fix for Heroku master #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  config.fog_provider = 'fog/aws'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
