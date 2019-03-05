b539bb0fe1309377eb3e23ac95badfc5ff31ce65
Taras <swex99@gmail.com>
2019-03-05 12:58:10 +0200

    fux big for heroku #4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
 
-  config.fog_provider = 'fog/aws'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/initializers/carrierwave.rb
+++
@@ -1,6 +0,0 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-
-config.fog_provider = 'fog/aws'
