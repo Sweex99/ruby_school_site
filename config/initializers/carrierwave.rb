771d521756d63610635a538a4032d3e13225d785
Taras <swex99@gmail.com>
2019-05-22 20:14:23 +0300

    Travis

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
     config.fog_provider= 'fog/aws'
     config.fog_credentials = {
         :provider              => 'AWS',
-        :aws_access_key_id     => 'AKIAJABHBLIUNZION3AA',
-        :aws_secret_access_key => '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',
+        :aws_access_key_id     => '',
+        :aws_secret_access_key => '',
         :region                => 'eu-north-1'
     }
     config.fog_directory = 'rschool'
