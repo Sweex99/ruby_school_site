9efc18575ea082b4e5e7d5e9ce10b82122026730
Taras <swex99@gmail.com>
2019-06-05 23:08:52 +0300

    fix bug by file_cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  config.telegram_updates_controller.session_store = :file_store
+
   config.action_mailer.default_url_options = { host: 'rschool.herokuapp.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
